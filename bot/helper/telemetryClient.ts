e337149cc4b00b2fd77c06d01878828226ce9778
zhijie <zhihuan@microsoft.com>
2023-02-13 16:01:50 +0800

    refactor: meaningful name for constants string

--- bot/helper/telemetryClient.ts
+++ bot/helper/telemetryClient.ts
@@ -1,5 +1,7 @@
 import { setup, TelemetryClient, defaultClient } from "applicationinsights";
-import { MiddlewareHandler } from "botbuilder";
+import { Activity, MiddlewareHandler } from "botbuilder";
+import { ActivityName, CommandId, Domains } from "./constant";
+import * as TelemetryConstants from "./telemetryConstants";
 
 export let TelemetryReporter: TelemetryClient;
 
@@ -14,26 +16,37 @@ export function setupApplicationInsights(): void {
 
 export const TelemetryMiddleware: MiddlewareHandler = async (context, next) => {
   await next();
-  const eventName = "Usage";
-  const source =
-    context.activity.name === "composeExtension/queryLink"
-      ? "linkUnfurling"
-      : context.activity.name === "composeExtension/submitAction" &&
-        context.activity.value.commandId === "createCard"
-      ? "createCard"
-      : "Unsupported";
-  const url = context.activity.value.url || context.activity.value.data.URL;
-  const urlType = url.includes("github.com")
-    ? "GitHub"
-    : url.includes(".visualstudio.com")
-    ? "AzDo"
-    : "Unsupported";
+  const eventName = TelemetryConstants.EventNames.usage;
   TelemetryReporter.trackEvent({
     name: eventName,
     properties: {
       id: context.activity.from.id,
-      source: source,
-      urlType: urlType,
+      source: resolveSource(context.activity),
+      urlType: resolveUrlType(context.activity),
     },
   });
 };
+
+function resolveSource(activity: Activity): TelemetryConstants.Source {
+  if (activity.name === ActivityName.queryLink) {
+    return TelemetryConstants.Source.linkUnfurling;
+  }
+  if (
+    activity.name === ActivityName.submitAction &&
+    activity.value?.commandId === CommandId.createCard
+  ) {
+    return TelemetryConstants.Source.createCard;
+  }
+  return TelemetryConstants.Source.unsupported;
+}
+
+function resolveUrlType(activity: Activity): TelemetryConstants.UrlType {
+  const url = activity.value.url || activity.value.data.URL;
+  if (url?.includes(Domains.github)) {
+    return TelemetryConstants.UrlType.github;
+  }
+  if (url?.includes(Domains.azdo)) {
+    return TelemetryConstants.UrlType.azdo;
+  }
+  return TelemetryConstants.UrlType.unsupported;
+}
